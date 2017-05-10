ed85f1b1041024705992a7ea928bb7cd4948a0e0
moumitacms <biswasarundhuti19@gmail.com>
2017-05-10 13:12:22 +0530

    Update getthreedaydata.sh

--- getthreedaydata.sh
+++ getthreedaydata.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 curl -o data.json -k https://ef5128a4-86b4-47b8-b14f-a0cd74abf0ef:MwN42ReQZ9@twcservice.mybluemix.net:443/api/weather/v1/geocode/12.97/77.59/forecast/daily/3day.json
+curl -o data.json -k https://ef5128a4-86b4-47b8-b14f-a0cd74abf0ef:MwN42ReQZ9@twcservice.mybluemix.net:443/api/weather/v1/geocode/19.07/72.87/forecast/daily/3day.json
